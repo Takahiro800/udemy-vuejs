3ffab58b804d6255d2c79d29c7123f0b068fe6e4
Takahiro Abe <hirotaka081630@gmail.com>
2021-07-26 07:58:16 +0900

    feat(141): カスタムディレクティブをローカルで定義する

--- server/udemy-sample/src/main.js
+++ server/udemy-sample/src/main.js
@@ -4,18 +4,7 @@ import LikeNumber from "./componentes/LikeNumber.vue";
 
 Vue.config.productionTip = false;
 Vue.component("LikeNumber", LikeNumber);
-Vue.directive("border", function(el, binding) {
-  el.style.border = "solid black 2px";
-  el.style.borderWidth = binding.value.width;
-  el.style.borderColor = binding.value.color;
-  el.style.borderStyle = binding.arg;
-  if (binding.modifiers.round) {
-    el.style.borderRadius = "0.5rem";
-  }
-  if (binding.modifiers.shadow) {
-    el.style.boxShadow = "0 2px 5px rgba(0,0,0,0.26)";
-  }
-});
+// Vue.directive("border", function(el, binding) {});
 
 new Vue({
   render: (h) => h(App),
