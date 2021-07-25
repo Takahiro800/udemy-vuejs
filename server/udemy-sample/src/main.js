c2be236f79bd6a139afcec68b0d0d98203c6e552
Takahiro Abe <hirotaka081630@gmail.com>
2021-07-25 18:17:52 +0900

    feat(~138): カスタムディレクティブの引数el,bindingについて

--- server/udemy-sample/src/main.js
+++ server/udemy-sample/src/main.js
@@ -4,6 +4,11 @@ import LikeNumber from "./componentes/LikeNumber.vue";
 
 Vue.config.productionTip = false;
 Vue.component("LikeNumber", LikeNumber);
+Vue.directive("border", function(el, binding) {
+  el.style.border = "solid black 2px";
+  el.style.borderWidth = binding.value.width;
+  el.style.borderColor = binding.value.color;
+});
 
 new Vue({
   render: (h) => h(App),
